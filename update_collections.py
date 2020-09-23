3cfa0f6f92d5736edc477b7766aa58738e36f878
Colin Sullender <shiruken343@gmail.com>
2020-09-23 12:24:39 -0400

    Created PlexMovieLibrary class to avoid repeatedly loading library contents

--- update_collections.py
+++ update_collections.py
@@ -8,21 +8,107 @@ PLEX_TOKEN = ""
 TRAKT_TOKEN = ""
 
 
-def get_imdb_id(movie):
-    """Directly queries Plex Media Server to get IMDb id from metadata
-    The new Plex Movie scanner uses a Plex-generated guid to identify
-    movie objects instead of the previously-used IMDb id. The PlexAPI
-    python module has yet to be updated to access the new `Guid` metadata
-    elements containing both the IMDb and TMDb ids.
-    """
-    url = f"{PLEX_URL}/library/metadata/{movie.ratingKey}?X-Plex-Token={PLEX_TOKEN}"
-    request = requests.get(url)
-    root = ElementTree.fromstring(request.text)
-    for guid in root.iter('Guid'):
-        if "imdb://" in guid.get("id"):
-            return guid.get("id").split("imdb://")[1]
+class PlexMovieLibrary(object):
 
-    return None
+    def __init__(self, plex_url, plex_token, library_name):
+        self.plex_url = plex_url
+        self.plex_token = plex_token
+        self.plex = PlexServer(self.plex_url, self.plex_token)
+        self.library = self.plex.library.section(library_name)
+        self.get_movie_library()
+
+    def get_movie_library(self):
+        """Populate dictionary mapping imdb_id to movie object for all movies in library"""
+        self.movies = {}
+        for movie in self.library.all():
+            imdb_id = self.get_imdb_id(movie)
+
+            if imdb_id:
+                self.movies[imdb_id] = movie
+            else:
+                print(f"Missing IMDb id for {movie.title} ({movie.year})")
+
+        print(f"\nFound {len(self.movies)} movies in Plex Library '{self.library.title}'\n")
+
+    def get_movies_in_collection(self, collection_name):
+        """Returns list of IMDb ids for all movies currently in a Plex collection"""
+        movies_in_collection = self.library.search(collection=collection_name)
+        imdb_ids = []
+        for movie in movies_in_collection:
+            imdb_id = list(self.movies.keys())[list(self.movies.values()).index(movie)]
+            imdb_ids.append(imdb_id)
+
+        if movies_in_collection:
+            print(f"{len(movies_in_collection)} movies are currently in the collection\n")
+        else:
+            print(f"The '{collection_name}' collection does not exist on Plex\n")
+        return imdb_ids
+
+    def get_imdb_id(self, movie):
+        """Directly queries Plex Media Server to get IMDb id from metadata
+        The new Plex Movie scanner uses a Plex-generated guid to identify
+        movie objects instead of the previously-used IMDb id. The PlexAPI
+        python module has yet to be updated to access the new `Guid` metadata
+        elements containing both the IMDb and TMDb ids.
+        """
+        url = f"{self.plex_url}/library/metadata/{movie.ratingKey}?X-Plex-Token={self.plex_token}"
+        request = requests.get(url)
+        root = ElementTree.fromstring(request.text)
+        for guid in root.iter('Guid'):
+            if "imdb://" in guid.get("id"):
+                return guid.get("id").split("imdb://")[1]
+
+        return None
+
+    def update_collection(self, trakt_list_url, collection_name):
+        """Create/update collection with movies in Trakt list"""
+        
+        print("===================================================================")
+        print(f"Updating '{collection_name}' collection")
+        print("===================================================================")
+
+        imdb_ids = self.get_movies_in_collection(collection_name)
+
+        # Add movies to collection
+        added_count = 0
+        missing = []
+        for movie in get_trakt_list(trakt_list_url):
+            imdb_id = movie["movie"]["ids"]["imdb"]
+            title = movie["movie"]["title"]
+            year = movie["movie"]["year"]
+
+            if imdb_id in self.movies:
+                if imdb_id not in imdb_ids:
+                    self.movies[imdb_id].addCollection(collection_name)
+                    print(f"  Added {title} ({year})")
+                    added_count += 1
+                else:
+                    imdb_ids.remove(imdb_id)
+            else:
+                missing.append(movie)
+
+        if added_count > 0:
+            print(f"\nAdded {added_count} movies to the collection\n")
+
+        # Remove movies from collection that are no longer on the list
+        for imdb_id in imdb_ids:
+            self.movies[imdb_id].removeCollection(collection_name)
+            print(f"  Removed {self.movies[imdb_id].title} ({self.movies[imdb_id].year})")
+
+        if len(imdb_ids) > 0:
+            print(f"\nRemoved {len(imdb_ids)} movies from the collection\n")
+
+        # Print missing movies
+        if len(missing) > 0:
+            print(f"Missing {len(missing)} movies:\n")
+            for entry in missing:
+                title = entry["movie"]["title"]
+                year = entry["movie"]["year"]
+                print(f"  {title} ({year})")
+        else:
+            print("Congrats! Your collection is complete!")
+
+        print("")
 
 
 def get_trakt_list(url):
@@ -38,84 +124,18 @@ def get_trakt_list(url):
     return request.json()
 
 
-def populate_collection(trakt_list_url, collection):
-    """Adds/removes movies to collection based on Trakt list"""
-    
-    plex = PlexServer(PLEX_URL, PLEX_TOKEN)
-    library = plex.library.section('Movies')
-
-    print("===================================================================")
-    print(f"Updating '{collection}' collection")
-    print("===================================================================")
-
-    # Create dictionary of all movies in Plex library {imdb_id: movie}
-    # Flag which movies are already in the collection
-    movies = {}
-    movies_in_collection = library.search(collection=collection)
-    movies_in_collection_imdb = []
-    for movie in library.all():
-        imdb_id = get_imdb_id(movie)
-
-        if imdb_id:
-            movies[imdb_id] = movie
-            if movie in movies_in_collection:
-                movies_in_collection_imdb.append(imdb_id)
-        else:
-            print(f"Missing IMDb id for {movie.title} ({movie.year})")
-
-    print(f"Found {len(movies)} movies in Plex. {len(movies_in_collection)} are currently in the collection.\n")
-
-    # Get list of movies from Trakt and add movies to the collection
-    added_count = 0
-    movies_missing = []
-    for movie in get_trakt_list(trakt_list_url):
-        imdb_id = movie["movie"]["ids"]["imdb"]
-        title = movie["movie"]["title"]
-        year = movie["movie"]["year"]
-
-        if imdb_id in movies:
-            if imdb_id not in movies_in_collection_imdb:
-                movies[imdb_id].addCollection(collection)
-                print(f"Added {title} ({year})")
-                added_count += 1
-            else:
-                movies_in_collection_imdb.remove(imdb_id)
-        else:
-            movies_missing.append(movie)
-
-    if added_count > 0:
-        print(f"Added {added_count} movies to the collection\n")
-
-    # Remove movies from collection that are no longer on the list
-    for imdb_id in movies_in_collection_imdb:
-        movies[imdb_id].removeCollection(collection)
-        print(f"Removed {movies[imdb_id].title} ({movies[imdb_id].year})")
-
-    if len(movies_in_collection_imdb) > 0:
-        print(f"Removed {len(movies_in_collection_imdb)} movies from the collection\n")
-
-    # Print missing movies
-    if len(movies_missing) > 0:
-        print(f"Missing {len(movies_missing)} movies:\n")
-        for entry in movies_missing:
-            title = entry["movie"]["title"]
-            year = entry["movie"]["year"]
-            print(f"  {title} ({year})")
-    else:
-        print("Congrats! Your collection is complete!")
-        
-    print("")
-
-
 if __name__ == "__main__":
+
+    library = PlexMovieLibrary(PLEX_URL, PLEX_TOKEN, "Movies")
+
     url_reddit_top250 = "https://trakt.tv/users/jay-greene/lists/reddit-top-250-2019-edition"
-    populate_collection(url_reddit_top250, "Reddit Top 250")
+    library.update_collection(url_reddit_top250, "Reddit Top 250")
 
     url_imdb_top250 = "https://trakt.tv/users/justin/lists/imdb-top-rated-movies"
-    populate_collection(url_imdb_top250, "IMDb Top 250")
+    library.update_collection(url_imdb_top250, "IMDb Top 250")
 
-    url_bestpicture_winners = "https://trakt.tv/users/thefork/lists/academy-awards-best-picture-winners"
-    populate_collection(url_bestpicture_winners, "Academy Award for Best Picture")
+    url_bestpicture = "https://trakt.tv/users/thefork/lists/academy-awards-best-picture-winners"
+    library.update_collection(url_bestpicture, "Academy Award for Best Picture")
 
-    url_disneyanimated = "https://trakt.tv/users/movistapp/lists/walt-disney-animated-feature-films"
-    populate_collection(url_disneyanimated, "Walt Disney Animation Studios")
+    url_disney = "https://trakt.tv/users/movistapp/lists/walt-disney-animated-feature-films"
+    library.update_collection(url_disney, "Walt Disney Animation Studios")
